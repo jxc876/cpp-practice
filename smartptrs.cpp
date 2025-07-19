3e0335af1341039f2ec53eead2860ad81fe77df3
Michael Ramirez <jxc876@gmail.com>
2025-07-19 09:18:49 -0500

    improve example

--- smartptrs.cpp
+++ smartptrs.cpp
@@ -1,8 +1,36 @@
 #include <cstdio>
 #include <boost/scoped_ptr.hpp>
 
+struct Person {
+    const char *name;
+
+    explicit Person(const char *name): name{name} {
+        printf("Creating Person: %s \n", name);
+    }
+
+    ~Person() {
+        printf("Deleting Person: %s \n", name);
+    }
+};
+
 int main() {
-    printf("Creating Smart Pointer\n");
-    boost::scoped_ptr<int> my_smart_ptr = boost::scoped_ptr<int>(new int(10));
-    printf("Smart Pointer was created\n");
+    typedef boost::scoped_ptr<Person> PersonSmartPtr;
+    printf("Demo of Smart Pointers\n");
+
+    // example one
+    {
+        boost::scoped_ptr<Person> person_ptr = boost::scoped_ptr(new Person("mike1"));
+    } // smart pointer deleted here, no need for delete
+
+    // example two, using a typedef alias
+    {
+        PersonSmartPtr person_ptr(new Person("mike2"));
+    }
+
+    // example three, using auto
+    {
+        auto person_ptr = boost::scoped_ptr(new Person("mike3"));
+    }
+
+    printf("Done\n");
 }
