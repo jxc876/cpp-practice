e9bba093f012e4f4bf49897c77e7568ac2d51e3b
Michael Ramirez <jxc876@gmail.com>
2025-07-19 09:37:09 -0500

    create function

--- smartptrs.cpp
+++ smartptrs.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <boost/scoped_ptr.hpp>
 
+void print_line();
+
 struct Person {
     const char *name;
 
@@ -19,11 +21,11 @@ struct Person {
 
 int main() {
     typedef boost::scoped_ptr<Person> PersonSmartPtr;
-    printf("Demo of Smart Pointers\n");
-    printf("-----------\n");
+    printf("A short demo showing Smart Pointers\n");
 
     // regular behavior
     {
+        print_line();
         auto *person = new Person("mike0");
         person->say_hello();
         delete person; // <== requires manual deletion
@@ -31,27 +33,29 @@ int main() {
 
     // example one
     {
+        print_line();
         boost::scoped_ptr<Person> person_ptr = boost::scoped_ptr(new Person("mike1"));
         person_ptr->say_hello();
     } // <== auto deleted here
 
-    printf("-----------\n");
-
     // example two, using a typedef alias
     {
+        print_line();
         PersonSmartPtr person_ptr(new Person("mike2"));
         person_ptr->say_hello();
     }
 
-    printf("-----------\n");
-
-
     // example three, using auto
     {
+        print_line();
         auto person_ptr = boost::scoped_ptr(new Person("mike3"));
         person_ptr->say_hello();
     }
 
-    printf("-----------\n");
+    print_line();
     printf("Done\n");
+}
+
+void print_line() {
+    printf("----------\n");
 }
