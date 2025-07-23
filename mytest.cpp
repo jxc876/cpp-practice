37b2d6af33641cd3a30865230be9af22f74dc4e2
Michael Ramirez <jxc876@gmail.com>
2025-07-22 20:22:37 -0500

    update test

--- mytest.cpp
+++ mytest.cpp
@@ -6,6 +6,7 @@ uint32_t factorial(uint32_t number) {
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( true );
     REQUIRE( factorial( 1) == 1 );
     REQUIRE( factorial( 2) == 2 );
     REQUIRE( factorial( 3) == 6 );
